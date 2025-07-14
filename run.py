2480cf7b36ec85e7a1f172f99fc14ecf738a4f65
davitshengelia <157176424+davitshengelia2407@users.noreply.github.com>
2025-07-14 13:53:57 +0400

    render problem

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from ext import app, db
 from routes import *
 
-from flask_migrate import upgrade
 
 if __name__ == "__main__":
-    with app.app_context():
-        upgrade()  # Applies migrations on startup (Render-safe fallback)
-
     app.run("0.0.0.0", port=5000)
